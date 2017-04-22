f3c6c06ea81e408643840336d5d045826867a75d
firefly1010 <wenzel.joubert@gmail.com>
2017-04-22 19:31:44 +0800

    add logging

--- n-open-routes.js
+++ n-open-routes.js
@@ -5,20 +5,33 @@ var app = module.exports = express.Router();
 
 // This is where all calls not needing auth goes
 
-app.get('/api/open',function(request, response){
-    azureTableHelper.get(request, response);
-})
+// app.get('/api/open',function(request, response){
+//     azureTableHelper.get(request, response);
+// })
 
-app.post('/api/open',function(request, response){
+// app.post('/api/open',function(request, response){
+//     azureTableHelper.post(request, response);
+// })
+
+// app.put('/api/open',function(request, response){
+//     azureTableHelper.put(request, response);
+// })
+
+// app.delete('/api/open',function(request, response){
+//      azureTableHelper.delete(request, response);
+// })
+
+// {
+// 	"tableName": "errorLogs",
+//     "partitionKey": "Date",
+//     "rowKey": "Time",
+//     "data":{
+//     		"error": "errors message"
+//     		}
+//   }
+//log errors 
+app.post('/api/log',function(request, response){
     azureTableHelper.post(request, response);
-})
-
-app.put('/api/open',function(request, response){
-    azureTableHelper.put(request, response);
-})
-
-app.delete('/api/open',function(request, response){
-     azureTableHelper.delete(request, response);
 })
 
 
